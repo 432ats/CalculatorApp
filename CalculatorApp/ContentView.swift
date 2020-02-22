9b7f195e65eae91b947ab3a99d8c0bf5f485df77
ats432 <ats.8060@gmail.com>
2020-02-22 20:51:11 +0900

    initial commit

---
+++ CalculatorApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorApp
+//
+//  Created by AtsushiShimizu on 2020/02/22.
+//  Copyright © 2020 AtsushiShimizu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
